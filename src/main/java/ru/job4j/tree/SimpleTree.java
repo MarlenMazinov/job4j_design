be4f31652027366ca00f3e547f075813387c6be1
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-01-02 17:10:37 +0300

    8. Реализовать собственную структуру данных - HashMap [#1008]

---
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -0,0 +1,39 @@
+package ru.job4j.tree;
+
+import java.util.*;
+
+public class SimpleTree<E> implements Tree<E> {
+    private final Node<E> root;
+
+    public SimpleTree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean rsl = false;
+        Optional<Node<E>> isParent = findBy(parent);
+        Optional<Node<E>> isChild = findBy(child);
+        if (isParent.isPresent() && isChild.isEmpty()) {
+            isParent.get().children.add(new Node<>(child));
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.value.equals(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+}
