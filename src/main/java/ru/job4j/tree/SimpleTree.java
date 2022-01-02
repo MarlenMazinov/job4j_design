9fbdea64d5d1940a32a51de23dc1282cf194c387
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-01-02 20:52:01 +0300

    2. Добавить метод boolean isBinary() [#1712]

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,6 +1,7 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
@@ -13,21 +14,30 @@ public class SimpleTree<E> implements Tree<E> {
     public boolean add(E parent, E child) {
         Optional<Node<E>> isParent = findBy(parent);
         Optional<Node<E>> isChild = findBy(child);
-        boolean rsl = isParent.isPresent() && isChild.isEmpty();
-        if (rsl) {
+        boolean condition = isParent.isPresent() && isChild.isEmpty();
+        if (condition) {
             isParent.get().children.add(new Node<>(child));
         }
-        return rsl;
+        return condition && isBinary();
     }
 
     @Override
     public Optional<Node<E>> findBy(E value) {
+        return findByPredicate(node -> node.value.equals(value));
+    }
+
+    public boolean isBinary() {
+        Optional<Node<E>> rsl = findByPredicate(node -> node.children.size() > 2);
+        return rsl.isEmpty();
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.value.equals(value)) {
+            if (condition.test(el)) {
                 rsl = Optional.of(el);
                 break;
             }
--- src/test/java/ru/job4j/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -2,33 +2,48 @@ package ru.job4j.tree;
 
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 
 public class SimpleTreeTest {
     @Test
-    public void when6ElFindLastThen6() {
+    public void whenAdd2Elements() {
         Tree<Integer> tree = new SimpleTree<>(1);
-        tree.add(1, 2);
-        tree.add(1, 3);
-        tree.add(1, 4);
-        tree.add(4, 5);
-        tree.add(5, 6);
-        assertThat(
-                tree.findBy(6).isPresent(),
-                is(true)
-        );
+        assertTrue(tree.add(1, 2));
+        assertTrue(tree.add(1, 3));
     }
 
     @Test
-    public void when6ElFindNotExitThenOptionEmpty() {
+    public void whenTryAddThirdElementInNode() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        assertTrue(tree.add(1, 2));
+        assertTrue(tree.add(1, 3));
+        assertFalse(tree.add(1, 4));
+    }
+
+    @Test
+    public void whenFindAllAddedElements() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        assertTrue(tree.findBy(4).isPresent());
+        assertTrue(tree.findBy(5).isPresent());
+    }
+
+    @Test
+    public void whenNotFindAbsentElements() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        assertFalse(tree.findBy(6).isPresent());
+    }
+
+    @Test
+    public void whenParentIsAbsentThenNotAdd() {
         Tree<Integer> tree = new SimpleTree<>(1);
         tree.add(1, 2);
-        assertThat(
-                tree.findBy(7).isPresent(),
-                is(false)
-        );
+        tree.add(2, 3);
+        assertFalse(tree.add(10, 5));
     }
 
     @Test
@@ -36,8 +51,7 @@ public class SimpleTreeTest {
         Tree<Integer> tree = new SimpleTree<>(1);
         tree.add(1, 2);
         tree.add(1, 3);
-        tree.add(1, 4);
-        tree.add(4, 5);
+        tree.add(3, 5);
         tree.add(5, 6);
         assertFalse(tree.add(2, 6));
     }
