7677da18ec4cf03e77682fd9c300c77b911688a4
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-01-05 15:28:48 +0300

    0.3. BufferedReader. [#252489]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogFilter {
+    public static List<String> filter(String file) {
+        List<String> rsl = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            in.lines().forEach(line -> {
+                String[] arr = line.split(" ");
+                if (!arr[arr.length - 1].equals("-") && arr[arr.length - 2].equals("404")) {
+                    rsl.add(line);
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("log.txt");
+        System.out.println(log);
+    }
+}
