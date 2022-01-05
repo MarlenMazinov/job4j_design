5f5c11d6c97a1d68f2da6ac4a13d6760fb7cfda4
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-01-05 10:58:24 +0300

    0.1. FileOutputStream. [#252488]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -6,6 +6,9 @@ public class ResultFile {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
             for (int i = 1; i < 10; i++) {
+                out.write("1 * ".getBytes());
+                out.write(Integer.toString(i).getBytes());
+                out.write(" = ".getBytes());
                 out.write(Integer.toString(i).getBytes());
                 out.write(System.lineSeparator().getBytes());
             }
