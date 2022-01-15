95a0f487cb73931fe19ea2caedd7f13d1bdf7a97
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-01-15 11:47:01 +0300

    7. Scanner [#504791]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -1,7 +1,6 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -11,9 +10,6 @@ public class CSVReader {
     public static void main(String[] args) throws Exception {
         if (args.length != 4) {
             throw new IllegalArgumentException("Number of arguments must be equal to four.");
-        }
-        if (Paths.get(args[0]).toFile().isDirectory()) {
-            throw new IllegalArgumentException("First argument isn't file.");
         }
         ArgsName argsName = ArgsName.of(args);
         handle(argsName);
@@ -41,18 +37,21 @@ public class CSVReader {
         int firstIndex = 0;
         int secondIndex = 0;
         String[] filterArr = argsName.get("filter").split(",");
+        int[] columnsArr = new int[filterArr.length];
         for (String field : listOfLists.get(0)) {
-            if (field.equals(filterArr[0])) {
-                firstIndex = listOfLists.get(0).indexOf(field);
-            }
-            if (field.equals(filterArr[1])) {
-                secondIndex = listOfLists.get(0).indexOf(field);
+            for (int i = 0; i < columnsArr.length; i++) {
+                if (field.equals(filterArr[i])) {
+                    columnsArr[i] = listOfLists.get(0).indexOf(field);
+                }
             }
         }
         StringJoiner joiner = new StringJoiner(System.lineSeparator());
         for (List<String> list : listOfLists) {
-            joiner.add(list.get(firstIndex)
-                    .concat(argsName.get("delimiter")).concat(list.get(secondIndex)));
+            StringJoiner lineJoiner = new StringJoiner(argsName.get("delimiter"));
+            for (int i : columnsArr) {
+                lineJoiner.add(list.get(i));
+            }
+            joiner.add(lineJoiner.toString());
         }
         joiner.add("");
         if (argsName.get("out").equals("stdout")) {
