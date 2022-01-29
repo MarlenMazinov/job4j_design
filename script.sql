39e16bec2aae8b2b68339d2e644eb0821f23119c
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-01-29 13:42:58 +0300

    0. Что такое база данных. [#6860 #257404]

--- script.sql
+++ script.sql
@@ -1,1 +1,10 @@
-select * from person
+create table persons(
+	id serial primary key,
+    name text,
+    phone bigserial,
+	sex varchar(255),
+	isMarried boolean
+);
+insert into persons(name, phone, sex, isMarried) values('Alexander', 79785562176, 'male', true);
+update persons set name = 'Oleg';
+delete from persons;
